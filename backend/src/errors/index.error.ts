4e00ca12562c18914a47d936c8975a346215865e
Sepehr <sepehr0107@gmail.com>
2024-10-02 23:42:03 +0200

    bootstrap, error handling, security

---
+++ backend/src/errors/index.error.ts
@@ -0,0 +1,5 @@
+import BadRequestError from "./badRequest.error"
+import ForbiddenError from "./forbidden"
+import NotFoundError from "./notFound.error"
+import InternalServerError from "./internalServer.error"
+export { BadRequestError, ForbiddenError, NotFoundError, InternalSeverError}
---
+++ backend/src/loader/bootstrap.ts
@@ -0,0 +1,12 @@
+import { bootstrapExpress } from "./app";
+import { logger } from '../config/logger';
+import { validateEnv } from "../config/env.config";
+import { connectToDB } from "../config/mongoose";
+
+export const bootstrap = async (app) => {
+    validateEnv()
+    await connectToDB();
+    bootstrapExpress(app);
+    logger.info('Express app initiated.')
+    
+};
